7e1f4ae5d6943c179a1bda4c97203f361ad55d18
vsyang <yang.vanessa85@gmail.com>
2023-09-12 22:29:47 -0600

    fixed prep 1 to camel case and completed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int percent = int.Parse(gradePercentage);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, you have passed!");
+        }
+        else
+        {
+            Console.WriteLine("Maybe it will be better next time.")
+        }
+
+
     }
 }

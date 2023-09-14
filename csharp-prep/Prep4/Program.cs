c4a82e57d4d30613a6cd25a52d6e215b83fa36f1
vsyang <yang.vanessa85@gmail.com>
2023-09-13 20:32:52 -0600

    prep 4 complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        int userInput =-1;
+        
+        while (userInput != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished. ");
+            string number = Console.ReadLine();
+            userInput = int.Parse(number);
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+        }
+
+        // Core requirement 1 sum or total
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}.");
+
+        // Core requirement 2 average
+        float average = ((float)sum) / numbers.Count;
+
+        Console.WriteLine($"The average is: {average}.");
+
+        // Core requirement 3 maximum
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The biggest number is: {max}.");
+
+
     }
 }

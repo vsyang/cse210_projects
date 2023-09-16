7fb1de38f2c38eb6323bd84f89dbdb57a8908e63
vsyang <yang.vanessa85@gmail.com>
2023-09-16 13:25:24 -0600

    Prepare 2 complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,59 @@
 using System;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+    }
+
+    public class Job
+    {
+        public string _jobTitle;
+        public string _company;
+        public int _startYear;
+        public int _endYear;
+
+        public void Display()
+        {
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+        }
+    }
+
+    public class Resume
+    {
+        public string _name;
+        public List<Job> _jobs = new List<Job>();
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine("Jobs:");
+            foreach (Job job in _jobs)
+            {
+                job.Display();
+            }
+        }
     }
 }

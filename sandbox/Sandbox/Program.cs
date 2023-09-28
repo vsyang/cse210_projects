30e1e2c246cd0894e5d98d1336c5d80150327401
vsyang <yang.vanessa85@gmail.com>
2023-09-28 15:17:56 -0600

    added some random notes for myself

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        //namespace > classes > members (method) > statements
+
+        //method always uses parenthesis
     }
 }

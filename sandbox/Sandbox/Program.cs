8ba85878a362e32a9c8d1edcdafc73d9f9b44d49
vsyang <yang.vanessa85@gmail.com>
2023-09-29 13:26:42 -0600

    new notes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -11,3 +11,9 @@ class Program
         //method always uses parenthesis
     }
 }
+
+//getters and settrs = add security to fields by encapsulation->accessors found within properties
+//properties = combine aspects of both fields and methods->share name with a field
+//get = used to return property value
+//set = used to assign a new value
+//value keyword = defines the value being assigned by the set parameter

94dee68b5f963e897d92fe4f482f5610221a4a52
vsyang <yang.vanessa85@gmail.com>
2023-09-30 00:13:27 -0600

    learning inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine("");
+        
+        Assignment a1 = new Assignment("Vanessa Yang", "Calculus");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Veronica Livingston", "Addition", "2", "1-12");
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Victoria Kim", "Puppies", "Why You Shouldn't Cook Your Dog");
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,15 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        string studentName = GetStudentName();
+        return $"Title \"{_title}\" by {studentName}";
+    }
+}

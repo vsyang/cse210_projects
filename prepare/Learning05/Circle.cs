dc4aec64927e1abacc5c07d8c0049fe01e13871c
vsyang <yang.vanessa85@gmail.com>
2023-10-10 21:57:40 -0600

    shape project done

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,17 @@
+using System.Net.NetworkInformation;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base (color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        double pi = Math.PI;
+        return pi * _radius * _radius;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("blue", 4);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("green", 2, 3);
+        shapes.Add(shape2);
+
+        Circle shape3  = new Circle("violet", 5);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
